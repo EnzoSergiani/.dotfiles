1db0d9b4c4e81725ea15edda27ca0d571013bcc5
EnzoSergiani <90188455+EnzoSergiani@users.noreply.github.com>
2024-06-01 15:30:01 +0200

    Update

--- scripts/.dev.sh
+++ scripts/.dev.sh
@@ -1,4 +1,2 @@
 #!/bin/bash    
 
-cd ~/Projets/
-find . -maxdepth 1 -type d  | fzf --height=100% --layout=reverse --border --margin=1 --padding=1 --preview 'tree {} -I ".*"' --bind='enter:execute(code {})+abort'
