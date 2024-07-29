c14f58fd2343a5e81711446ec37654e59118af6f
EnzoSergiani <enzo.sergiani@protonmail.com>
2024-07-29 11:41:06 +0200

    add wofi/scripts

---
+++ wofi/scripts/power_menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+chosen=$(echo -e "⏻ Éteindre\n Redémarrer\n Verrouiller" | wofi --dmenu --prompt "Effectuer une action" --style ~/.dotfiles/wofi/scripts/power_menu.css --width 800 --height 135)
+
+case "$chosen" in
+    "⏻ Éteindre")
+        systemctl poweroff
+        ;;
+    " Redémarrer")
+        systemctl reboot
+        ;;
+    " Verrouiller")
+        hyprlock
+        ;;
+    *)
+        ;;
+esac
+
