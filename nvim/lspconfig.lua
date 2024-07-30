6ab096a752e672a50896abfe5965575f8dd87eac
EnzoSergiani <enzo.sergiani@protonmail.com>
2024-07-30 16:37:18 +0200

    add nvim files

---
+++ nvim/lspconfig.lua
@@ -0,0 +1,34 @@
+-- EXAMPLE
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require("lspconfig")
+
+local servers = {
+	"html", -- HTML
+	"cssls", -- CSS
+	"tsserver", -- JavaScript and TypeScript
+	"bashls", -- Bash
+	"clangd", -- C and C++
+	"pyright", -- Python
+	"texlab", -- LaTeX
+	"marksman", -- Markdown
+	"lua_ls", -- Lua
+}
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		on_init = on_init,
+		capabilities = capabilities,
+	})
+end
+
+-- typescript
+lspconfig.tsserver.setup({
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
+})
