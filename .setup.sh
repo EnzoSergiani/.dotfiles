d3ed057ad5d086bd15061b1953b5ca7c8c1314bf
EnzoSergiani <enzo.sergiani@protonmail.com>
2024-08-05 18:19:10 +0200

    update

--- .setup.sh
+++ .setup.sh
@@ -3,55 +3,64 @@
 set -e
 
 # name of packages to install
-PACKAGES=(  
-    "alacritty"
-    "alsa-utils"
-    "base"
-    "base-devel"
-    "blueberry"
-    "bluez-utils"
-    "bluez"
-    "brightnessctl"
-    "code"
-    "discord"
-    "dunst"
-    "efibootmgr"
-    "eog"
-    "firefox"
-    "git"
-    "gnome-calculator"
-    "gnome-disk-utility"
-    "gnome-font-viewer"
-    "gnome-system-monitor"
-    "grub"
-    "hyprland"
-    "hyprlock"
-    "hyprpaper"
-    "iwd"
-    "man-db"
-    "nautilus"
-    "neofetch"
-    "neovim"
-    "neotwork-manager-applet"
-    "networkmanager"
-    "npm"
-    "pavucontrol"
-    "pipewire"
-    "pipewire-alsa"
-    "pipewire-audio"
-    "pipewire-jack"
-    "pipewire-pulse"
-    "ripgrep"
-    "signal-desktop"
-    "unzip"
-    "virtualbox"
-    "vlc"
-    "waybar"
-    "wget"
-    "wireplumber"
-    "wmctrl"
-    "wofi"
-    "xdg-user-dirs"
+PACKAGES=(
+  "alsa-utils"
+  "baobab"
+  "base"
+  "base-devel"
+  "blueberry"
+  "bluez-utils"
+  "bluez"
+  "brightnessctl"
+  "discord"
+  "dmd"
+  "dmd-docs"
+  "dunst"
+  "efibootmgr"
+  "eog"
+  "fastfetch"
+  "firefox"
+  "fprintd"
+  "gimp"
+  "git"
+  "gnome-calculator"
+  "gnome-disk-utility"
+  "gnome-font-viewer"
+  "gnome-system-monitor"
+  "gnome-text-editor"
+  "grub"
+  "htop"
+  "hyprland"
+  "hyprlock"
+  "hyprpaper"
+  "inkscape"
+  "iwd"
+  "kitty"
+  "man-db"
+  "nautilus"
+  "neofetch"
+  "neovim"
+  "network-manager-applet"
+  "networkmanager"
+  "npm"
+  "pavucontrol"
+  "pipewire"
+  "pipewire-alsa"
+  "pipewire-audio"
+  "pipewire-jack"
+  "pipewire-pulse"
+  "ripgrep"
+  "signal-desktop"
+  "stow"
+  "unzip"
+  "virtualbox"
+  "vlc"
+  "waybar"
+  "wget"
+  "wireplumber"
+  "wmctrl"
+  "wofi"
+  "xdg-user-dirs"
 )
 
 # update system
@@ -59,54 +68,58 @@ sudo pacman -Syu --noconfirm
 
 # installation of packages
 for package in "${PACKAGES[@]}"; do
-    sudo pacman -S --needed --noconfirm "$packages"
+  sudo pacman -S --needed --noconfirm "$package"
 done
 
 # yay
-if git clone https://aur.archlinux.org/yay.git /opt/; then
-  cd yay
-  makepkg -si
-else
-  echo "Failed to clone yay repository."
-  exit 1
-if
+#if git clone https://aur.archlinux.org/yay.git /opt/; then
+#  cd yay
+#  makepkg -si
+#else
+#  echo "Failed to clone yay repository."
+#  exit 1
+#fi
 
 # bashrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 
 # Nvim
-rm -rf ~/.config/nvim
+rm -rf ~/.config/nvim && rm -rf ~/.cache/nvim
+
 if git clone https://github.com/NvChad/starter ~/.config/nvim; then
-    ln -sf ~/.dotfiles/nvim/chadrc.lua ~/.config/nvim/lua/chadrc.lua
-    ln -sf ~/.dotfiles/nvim/mappings.lua ~/.config/nvim/lua/mappings.lua
-    ln -sf ~/.dotfiles/nvim/options.lua ~/.config/nvim/lua/options.lua
-    ln -sf ~/.dotfiles/nvim/init.lua ~/.config/nvim/lua/plugin/init.lua
+  echo "NvChad repository successfully cloned."
 else
-    echo "Failed to clone NvChad repository."
-    exit 1
+  echo "Failed to clone NvChad repository."
+  exit 1
 fi
 
-# alacritty
-mkdir -p ~/.config/alacritty 
-ln -sf ~/.dotfiles/alacritty/alacritty.toml ~/.config/alacritty/
+#stow -d . -t ~/.config nvim
 
-# dunst
-mkdir -p ~/.config/dunst 
-ln -sf ~/.dotfiles/dunst/dunstrc ~/.config/dunst/
+mkdir -p ~/.config/{dunst,fastfetch,hypr,kitty,nvim/scripts,power_menu,waybar,wofi}
 
-# hypr
-mkdir -p ~/.config/hypr
+# Créer les liens symboliques
+ln -sf ~/.dotfiles/dunst/dunstrc ~/.config/dunst/dunstrc
+
+ln -sf ~/.dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
+
 ln -sf ~/.dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -sf ~/.dotfiles/hypr/hyprpaper.conf ~/.config/hypr/
+ln -sf ~/.dotfiles/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
+ln -sf ~/.dotfiles/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 
-# waybar
-ln -sf ~/.dotfiles/waybar/config.jsonc ~/.config/waybar/ 
-ln -sf ~/.dotfiles/waybar/style.css ~/.config/waybar/
+ln -sf ~/.dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
-# wofi
-mkdir -p ~/.config/wofi 
-ln -sf ~/.dotfiles/wofi/style.css ~/.config/wofi/ 
-ln -sf ~/.dotfiles/wofi/config ~/.config/wofi/
+ln -sf ~/.dotfiles/nvim/init.lua ~/.config/nvim/lua/plugins/init.lua
+ln -sf ~/.dotfiles/nvim/lazy.lua ~/.config/nvim/lua/configs/lazy.lua
+ln -sf ~/.dotfiles/nvim/chadrc.lua ~/.config/nvim/lua/chadrc.lua
+ln -sf ~/.dotfiles/nvim/conform.lua ~/.config/nvim/lua/configs/conform.lua
+ln -sf ~/.dotfiles/nvim/options.lua ~/.config/nvim/lua/options.lua
+ln -sf ~/.dotfiles/nvim/mappings.lua ~/.config/nvim/lua/mappings.lua
+ln -sf ~/.dotfiles/nvim/lspconfig.lua ~/.config/nvim/lua/configs/lspconfig.lua
+
+ln -sf ~/.dotfiles/waybar/config.jsonc ~/.config/waybar/config.jsonc
+ln -sf ~/.dotfiles/waybar/style.css ~/.config/waybar/style.css
+
+ln -sf ~/.dotfiles/wofi/config ~/.config/wofi/config
+ln -sf ~/.dotfiles/wofi/style.css ~/.config/wofi/style.css
 
 echo "Installation and configuration done."
-
