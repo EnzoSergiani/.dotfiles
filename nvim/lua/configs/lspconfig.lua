b4de85f46114644d6a8bbabec7e155f748238744
Enzo Sergiani <enzo.sergiani@protonmail.com>
2024-11-08 16:23:15 +0100

    update

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,32 +3,25 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 
 local servers = {
-	"html", -- HTML
-	"cssls", -- CSS
-	"tsserver", -- JavaScript and TypeScript
-	"bashls", -- Bash
-	"clangd", -- C and C++
-	"pyright", -- Python
-	"texlab", -- LaTeX
-	"marksman", -- Markdown
-	"lua_ls", -- Lua
+  "html", -- HTML
+  "cssls", -- CSS
+  "ts_ls", -- JavaScript and TypeScript
+  "bashls", -- Bash
+  "clangd", -- C and C++
+  "pyright", -- Python
+  "texlab", -- LaTeX
+  "marksman", -- Markdown
+  "lua_ls", -- Lua
 }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = on_attach,
-		on_init = on_init,
-		capabilities = capabilities,
-	})
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
 end
-
--- typescript
-lspconfig.tsserver.setup({
-	on_attach = on_attach,
-	on_init = on_init,
-	capabilities = capabilities,
-})
