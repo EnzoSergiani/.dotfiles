4d73802caba08c7d989ff49198961c8d1e1319cb
Enzo Sergiani <enzo.sergiani@protonmail.com>
2025-11-23 19:27:44 +0100

    init xdg

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,6 +109,22 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
+  environment.sessionVariables = {
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_STATE_HOME = "$HOME/.local/state";
+
+    XDG_DESKTOP_DIR="$HOME/desktop";
+    XDG_DOWNLOAD_DIR="$HOME/downloads";
+    XDG_TEMPLATES_DIR="$HOME/templates";
+    XDG_PUBLICSHARE_DIR="$HOME/public";
+    XDG_DOCUMENTS_DIR="$HOME/documents";
+    XDG_MUSIC_DIR="$HOME/music";
+    XDG_PICTURES_DIR="$HOME/pictures";
+    XDG_VIDEOS_DIR="$HOME/videos";
+  };
+
 
   # Fonts
   fonts.packages = with pkgs; [
