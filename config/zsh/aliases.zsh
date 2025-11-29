6ae4839ef53ed53376ddac61983a5bb839609ade
EnzoSergiani <enzo.sergiani@protonmail.com>
2025-11-29 19:51:36 +0100

    feat(nvim): remove kitty padding in nvim

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -10,3 +10,15 @@ alias rebuild="sudo nixos-rebuild switch";
 alias update="sudo nixos-rebuild switch --upgrade";
 alias cleanup="sudo nix-collect-garbage -d";
 alias nix-search="nix search nixpkgs";
+
+nvim() {
+  if [[ "$TERM" == "xterm-kitty" ]]; then
+    kitty @ set-spacing padding=0
+    command nvim "$@"
+    local exit_code=$?
+    kitty @ set-spacing padding=4
+    return $exit_code
+  else
+    command nvim "$@"
+  fi
+}
