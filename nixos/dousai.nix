802f270f07cf8dda00b0be04d189efaee0cf9077
EnzoSergiani <enzo.sergiani@protonmail.com>
2025-11-29 18:56:17 +0100

    feat(nixos): add directories

--- nixos/dousai.nix
+++ nixos/dousai.nix
@@ -13,6 +13,29 @@ in
     value.source = "${dotfiles}/config/${name}";
   }) configDirs);
 
+  xdg = {
+    enable = true;   
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      desktop = "${config.home.homeDirectory}/desktop";
+      documents = "${config.home.homeDirectory}/documents";
+      download = "${config.home.homeDirectory}/downloads";
+      music = "${config.home.homeDirectory}/music";
+      pictures = "${config.home.homeDirectory}/pictures";
+      publicShare = "${config.home.homeDirectory}/public";
+      templates = "${config.home.homeDirectory}/templates";
+      videos = "${config.home.homeDirectory}/videos";
+    };
+  };
+
+  home.activation.createCustomDirectories = config.lib.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD mkdir -p $VERBOSE_ARG \
+      ${config.home.homeDirectory}/dev/projects \
+      ${config.home.homeDirectory}/dev/vm \
+      ${config.home.homeDirectory}/latex
+  '';
+
   home.packages = with pkgs; [
     btop
     cava
@@ -43,6 +66,7 @@ in
     syntaxHighlighting.enable = true;
     initContent = builtins.readFile "${dotfiles}/config/zsh/.zshrc";
   };
+
   programs.git = {
     enable = true;
     userName  = "EnzoSergiani";
