55edc307a46673382511af46a5235ab112069668
Enzo Sergiani <enzo.sergiani@protonmail.com>
2025-12-27 14:23:01 +0100

    fix(zsh): update aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -4,13 +4,22 @@ alias ls="lsd"
 alias ll="lsd -lah"
 alias ..="cd .."
 alias rm="trash-put"
+alias -- '-'='cd -'
+
 alias grep="grep --color=auto"
 alias open="xdg-open"
+alias cls="clear"
+alias now="date '+%Y-%m-%d %H:%M:%S'"
 
-alias rebuild="sudo nixos-rebuild switch"
-alias update="sudo nixos-rebuild switch --upgrade"
-alias cleanup="sudo nix-collect-garbage -d"
+alias nix-rebuild="sudo nixos-rebuild switch"
+alias nix-upgrade="sudo nixos-rebuild switch --upgrade"
+alias nix-clean="sudo nix-collect-garbage -d"
 alias nix-search="nix search nixpkgs"
+alias nix-install="nix-env -iA nixos."
+alias nix-upgrade="nix-env -u"
+alias nix-remove="nix-env -e"
+alias nix-list="nix-env -q"
+alias nix-optimize="sudo nix-collect-garbage -d && sudo nix-store --optimise"
 
 nvim() {
   if [[ "$TERM" == "xterm-kitty" ]]; then
