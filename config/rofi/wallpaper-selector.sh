cdfce77d0d888e14a52658ce4832adcb945bd5a0
EnzoSergiani <enzo.sergiani@protonmail.com>
2026-01-01 15:34:05 +0100

    fix(hypr): correct selection wallpaper

--- config/rofi/wallpaper-selector.sh
+++ config/rofi/wallpaper-selector.sh
@@ -2,46 +2,41 @@
 
 WALL_DIR="$HOME/.dotfiles/wallpapers"
 CACHE_DIR="$HOME/.cache/rofi-wallpaper"
-HYPRPAPER_CONF="$HOME/.config/hypr/hyprpaper.conf"
+CURRENT_WALL_LINK="$HOME/.cache/current_wallpaper"
 
 mkdir -p "${CACHE_DIR}"
 
 shopt -s nullglob
 for imagen in "$WALL_DIR"/*.jpg "$WALL_DIR"/*.jpeg "$WALL_DIR"/*.png "$WALL_DIR"/*.webp; do
-    if [ -f "$imagen" ]; then
-        nombre_archivo=$(basename "$imagen")
-        if [ ! -f "${CACHE_DIR}/${nombre_archivo}" ]; then
-            magick "$imagen" -strip -thumbnail 500x500^ -gravity center -extent 500x500 "${CACHE_DIR}/${nombre_archivo}" 2>/dev/null
-        fi
+  if [ -f "$imagen" ]; then
+    nombre_archivo=$(basename "$imagen")
+    if [ ! -f "${CACHE_DIR}/${nombre_archivo}" ]; then
+      magick "$imagen" -strip -thumbnail 500x500^ -gravity center -extent 500x500 "${CACHE_DIR}/${nombre_archivo}" 2>/dev/null
     fi
+  fi
 done
 shopt -u nullglob
 
 wall_selection=$(
-    shopt -s nullglob
-    for imagen in "$WALL_DIR"/*.{jpg,jpeg,png,webp}; do
-        if [ -f "$imagen" ]; then
-            nombre_archivo=$(basename "$imagen")
-            nombre_sin_extension="${nombre_archivo%.*}"
-            echo -en "$nombre_archivo\x00icon\x1f${CACHE_DIR}/$nombre_archivo\n"
-        fi
-    done | sort | rofi -dmenu -theme ~/.dotfiles/config/rofi/wallpaper-theme.rasi -p "WALLPAPER"
-    shopt -u nullglob
+  shopt -s nullglob
+  for imagen in "$WALL_DIR"/*.{jpg,jpeg,png,webp}; do
+    if [ -f "$imagen" ]; then
+      nombre_archivo=$(basename "$imagen")
+      nombre_sin_extension="${nombre_archivo%.*}"
+      echo -en "$nombre_archivo\x00icon\x1f${CACHE_DIR}/$nombre_archivo\n"
+    fi
+  done | sort | rofi -dmenu -theme ~/.dotfiles/config/rofi/wallpaper-theme.rasi -p "WALLPAPER"
+  shopt -u nullglob
 )
 
 if [[ -n "$wall_selection" ]]; then
-    WALLPAPER_PATH="${WALL_DIR}/${wall_selection}"
+  WALLPAPER_PATH="${WALL_DIR}/${wall_selection}"
 
-    hyprctl hyprpaper preload "$WALLPAPER_PATH"
-    hyprctl hyprpaper wallpaper ",$WALLPAPER_PATH"
-    hyprctl hyprpaper unload unused
+  ln -sf "$WALLPAPER_PATH" "$CURRENT_WALL_LINK"
 
-    cat > "$HYPRPAPER_CONF" << EOF
-preload = $WALLPAPER_PATH
-wallpaper = ,$WALLPAPER_PATH
-splash = false
-ipc = on
-EOF
+  hyprctl hyprpaper unload all
+  hyprctl hyprpaper preload "$CURRENT_WALL_LINK"
+  hyprctl hyprpaper wallpaper ",$CURRENT_WALL_LINK"
 fi
 
 exit 0
