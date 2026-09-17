ee4058ab4517c219fcc9bfb9ef5ed7d12c2c27d9
EnzoSergiani <enzo.sergiani@protonmail.com>
2026-09-17 17:06:44 +0200

    fix(nvim): remove copilot

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -123,18 +123,6 @@ return {
   },
 
   {
-    "github/copilot.vim",
-    lazy = false,
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_filetypes = {
-        ["*"] = true,
-      }
-    end,
-  },
-
-  {
     "lervag/vimtex",
     ft = { "tex" },
     config = function()
